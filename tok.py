c42b842f2c8b616ba8040d374336908c7267ec5a
Robertdebrus <robert@peterwallace.org>
2018-06-19 14:31:52 -0400

    Update tok.py

--- tok.py
+++ tok.py
@@ -1,27 +1,33 @@
+
+
 class Tokenizer:
 
      def __init__(self):
 #        print('here!')
         ad = dict()
         self.ad = ad
-
+        num = 0
+        self.num = num
+        adindex = 0
+        self.adindex = adindex
 
      def train(self, msg):
         print(msg)
 
-        msg.spilt(' ')
+        msg = msg.split(' ')
         print(msg)
 
-        try:
-            print(self.ad[msgno])
-        except KeyError as ex:
-            self.ad[msgno] = adindex
-            adindex +=1
-            print(self.ad[msgno])
-
-        print('got here!')
-
+        for x in msg:
+            msgno = msg[self.num]
+            print(msg[self.num])
+            try:
+                print(self.ad[msgno])
+            except KeyError as ex:
+                self.ad[msgno] = self.adindex
+                print(self.ad[msgno])
+            self.adindex += 1
+            self.num += 1
 
 mytok = Tokenizer()
 
-mytok.train("the,movie,was,good,positive")
+mytok.train("the movie was good positive")
